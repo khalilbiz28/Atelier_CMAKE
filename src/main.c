ea9a9a90f852baaa128b9c8bcfb3ec2c7ef12694
khalilbiz28 <129905670+khalilbiz28@users.noreply.github.com>
2025-06-04 10:33:40 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,44 +3,41 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]) {
-    if (argc == 4) {
+int main(int argc, char *argv[]){
+    if (argc == 4)
+    {
         char* op = argv[1];
-        double a = atof(argv[2]);
-        double b = atof(argv[3]);
+        char* a = argv[2];
+        char* b = argv[3];
         double r = 0;
-
         if (strcmp(op, "add") == 0) {
-            r = _add(a, b);
-        } else if (strcmp(op, "sub") == 0) {
-            r = _sub(a, b);
-        } else if (strcmp(op, "mul") == 0) {
-            r = _mul(a, b);
-        } else if (strcmp(op, "div") == 0) {
-            r = _div(a, b);
-        } else {
-            printf("Erreur de parametres\n");
-            return 1;
+            r = _add(atof(a), atof(b));
+            printf("%lf", r);
         }
-        printf("%lf\n", r);
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
     }
-    else if (argc == 3) {
-        char* op = argv[1];
-        double a = atof(argv[2]);
-        double r = 0;
-
-        if (strcmp(op, "car") == 0) {
-            r = _car(a);
-            printf("%lf\n", r);
-        } else {
-            printf("Erreur de parametres\n");
-            return 1;
-        }
+    else if (argc == 3 && strcmp(argv[1], "car") == 0)
+    {
+        int a = atoi(argv[2]);
+        int r = _car(a);
+        printf("%d", r);
     }
     else {
-        printf("Erreur de parametres\n");
-        return 1;
+        printf("Erreur de parametres");
     }
-
     return 0;
 }
